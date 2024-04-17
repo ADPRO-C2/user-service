be67f0ffb143059ba9388d11414884ea5c35e3d8
riozulfandy <rio.zulfandy07@gmail.com>
2024-04-17 11:17:28 +0800

    first commit

---
+++ src/main/java/com/example/secondtreasurebe/SecondtreasureBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.secondtreasurebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondtreasureBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondtreasureBeApplication.class, args);
+    }
+
+}
