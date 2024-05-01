4b98b0ee1787fea1754a621d36c7e172d31b8e65
riozulfandy <rio.zulfandy07@gmail.com>
2024-05-02 02:31:05 +0700

    [REFACTOR] Fix aplication test

--- src/test/java/com/example/user/UserApplicationTests.java
+++ src/test/java/com/example/user/UserApplicationTests.java
@@ -1,14 +1,6 @@
 package com.example.user;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserApplicationTests {
-
-    @Test
-    void contextLoads() {
-        UserApplication.main(new String[] {});
-    }
-
-}
+class UserApplicationTests {}
