113da6cdc10c3f92bf818d61b23442cb8c01e0c6
riozulfandy <rio.zulfandy07@gmail.com>
2024-05-10 06:02:09 +0700

    [RED] Add test for AuthResponse

---
+++ src/test/java/com/example/user/model/AuthResponseTest.java
@@ -0,0 +1,18 @@
+package com.example.user.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AuthResponseTest {
+
+    @Test
+    public void testAuthResponse() {
+        String expectedToken = "testToken";
+        String expectedMessage = "testMessage";
+
+        AuthResponse authResponse = new AuthResponse(expectedToken, expectedMessage);
+
+        assertEquals(expectedToken, authResponse.getToken());
+        assertEquals(expectedMessage, authResponse.getMessage());
+    }
+}
